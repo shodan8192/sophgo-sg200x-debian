ef1e501cbad6c5e151533ab97e8a26a34acfec4a
scpcom <scpcom@gmx.de>
2025-01-25 21:26:07 +0100

    duos: enable device-key, ethernet-builtin and load-systemko

--- configs/duos/settings.mk
+++ configs/duos/settings.mk
@@ -10,6 +10,9 @@ PARTITION_FILE=partition_$(STORAGE_TYPE).xml
 
 PACKAGES += " duo-pinmux wireless-regdb wpasupplicant cvi-pinmux-cv181x bluez"
 
+IMAGE_ADDITIONS += "device-key"
+IMAGE_ADDITIONS += "ethernet-builtin"
+IMAGE_ADDITIONS += "load-systemko"
 IMAGE_ADDITIONS += "aic8800-firmware"
 IMAGE_ADDITIONS += "ethernet-leds"
 IMAGE_ADDITIONS += "usb-switch"
