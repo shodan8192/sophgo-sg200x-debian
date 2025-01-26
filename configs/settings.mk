5f43a7b4ee96f0a91acd6115d8544b6a1f687ecd
scpcom <scpcom@gmx.de>
2025-01-26 21:26:31 +0100

    add unzip to target image

--- configs/settings.mk
+++ configs/settings.mk
@@ -2,6 +2,6 @@ KERNELREV="4"
 FSBLVERSION=1.1.0
 OSDRVVERSION=2024.10.14
 MIDDLEWAREVERSION=2024.10.14
-PACKAGES="busybox-static ca-certificates debian-archive-keyring dosfstools binutils file tree sudo bash-completion u-boot-menu openssh-server network-manager dnsmasq-base libpam-systemd ppp libengine-pkcs11-openssl iptables systemd-timesyncd vim usbutils parted exfatprogs systemd-sysv i2c-tools net-tools ifupdown ethtool avahi-utils sudo gnupg rsync gpiod u-boot-tools libubootenv-tool"
+PACKAGES="busybox-static ca-certificates debian-archive-keyring dosfstools binutils file tree sudo bash-completion u-boot-menu openssh-server network-manager dnsmasq-base libpam-systemd ppp libengine-pkcs11-openssl iptables systemd-timesyncd vim usbutils parted exfatprogs systemd-sysv i2c-tools net-tools ifupdown ethtool avahi-utils sudo gnupg rsync gpiod u-boot-tools libubootenv-tool unzip"
 
 IMAGE_ADDITIONS="usb-gadget"
