9f8e4f1a9553da30bd3bc48d04f667f069c546da
scpcom <scpcom@gmx.de>
2025-01-26 14:03:12 +0100

    package middleware and osdrv

--- configs/settings.mk
+++ configs/settings.mk
@@ -1,5 +1,7 @@
 KERNELREV="3"
 FSBLVERSION=1.1.0
+OSDRVVERSION=2024.10.14
+MIDDLEWAREVERSION=2024.10.14
 PACKAGES="busybox-static ca-certificates debian-archive-keyring dosfstools binutils file tree sudo bash-completion u-boot-menu openssh-server network-manager dnsmasq-base libpam-systemd ppp libengine-pkcs11-openssl iptables systemd-timesyncd vim usbutils parted exfatprogs systemd-sysv i2c-tools net-tools ifupdown ethtool avahi-utils sudo gnupg rsync gpiod u-boot-tools libubootenv-tool"
 
 IMAGE_ADDITIONS="usb-gadget"
