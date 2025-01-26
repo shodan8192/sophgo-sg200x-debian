b29927c8155e68146e1bdfacb567210e6bd55244
scpcom <scpcom@gmx.de>
2025-01-26 19:15:43 +0100

    device-config: add fw_vcodec

--- scripts/addons/device-config/addon.mk
+++ scripts/addons/device-config/addon.mk
@@ -6,6 +6,8 @@ $(BUILDDIR)/device-config-stamp:
 	@cp -a addons/device-config/device-config*.service /rootfs/etc/systemd/system/
 	@mkdir -pv /rootfs/mnt/
 	@rsync -avpPxH addons/device-config/overlay/mnt/ /rootfs/mnt/
+	@mkdir -pv /rootfs/usr/share/fw_vcodec/
+	@rsync -avpPxH addons/device-config/overlay/usr/share/fw_vcodec/ /rootfs/usr/share/fw_vcodec/
 	@mkdir -p /rootfs/tmp/install/
 	@echo " device-config" >> /rootfs/tmp/install/systemd-enable
 	@touch $@
