ddfb6ea30137b596473d6d53e6ca8afa60eecb6a
scpcom <scpcom@gmx.de>
2025-02-13 19:22:47 +0100

    addons: gadget-nic: add support for usb.rndis0

--- scripts/addons/gadget-nic/addon.mk
+++ scripts/addons/gadget-nic/addon.mk
@@ -1,10 +1,13 @@
 $(BUILDDIR)/gadget-nic-stamp:
 	@echo "$(COLOUR_GREEN)Installing gadget-nic for $(BOARD)$(END_COLOUR)"
-	@mkdir -pv /rootfs/boot/
-	@touch /rootfs/boot/usb.rndis
+	@if [ "X$(findstring kvm,$(VARIANT))" = "X" ]; then \
+		@mkdir -pv /rootfs/boot/ ; \
+		@touch /rootfs/boot/usb.rndis ; \
+	fi
 	@mkdir -pv /rootfs/etc/init.d/
 	@cp -a addons/gadget-nic/S30gadget_nic /rootfs/etc/init.d/
 	@chmod +x /rootfs/etc/init.d/S30gadget_nic
+	@ln -s S30gadget_nic /rootfs/etc/init.d/S30rndis
 	@cp -a addons/gadget-nic/gadget-nic*.service /rootfs/etc/systemd/system/
 	@mkdir -p /rootfs/tmp/install/
 	@echo " gadget-nic" >> /rootfs/tmp/install/systemd-enable
