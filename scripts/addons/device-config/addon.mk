1681c3a03823c0e4cc58b6e14720d4dbdde6066a
scpcom <scpcom@gmx.de>
2025-03-02 19:04:02 +0100

    package sensor-config

--- scripts/addons/device-config/addon.mk
+++ scripts/addons/device-config/addon.mk
@@ -14,14 +14,43 @@ $(BUILDDIR)/firmware-vcodec-package-stamp:
 
 firmware-vcodec: $(BUILDDIR)/firmware-vcodec-package-stamp
 
-$(BUILDDIR)/device-config-stamp: firmware-vcodec
+$(BUILDDIR)/sensor-config-install-stamp:
+	@mkdir -pv /rootfs/mnt/cfg/param/
+	@mkdir -pv /rootfs/mnt/data/
+	@if [ "$(BOARD)" = "licheervnano" ]; then \
+		cp -p addons/device-config/overlay/mnt/cfg/param/sipeed_gc4653_30fps_202403261356.bin /rootfs/mnt/cfg/param/cvi_sdr_bin ; \
+	elif [ "$(BOARD)" = "duo256" ]; then \
+		cp -p addons/device-config/overlay/mnt/cfg/param/cvi_sdr_bin_GC2083 /rootfs/mnt/cfg/param/cvi_sdr_bin && \
+		cp -p addons/device-config/overlay/mnt/data/sensor_cfg_GC2083.ini /rootfs/mnt/data/sensor_cfg.ini ; \
+	fi
+	@touch $@
+
+$(BUILDDIR)/sensor-config-package-stamp:
+	@echo "$(COLOUR_GREEN)Packaging sensor-config for $(BOARD)$(END_COLOUR)"
+	@mkdir -p $(BUILDDIR)/package/sensor-config-$(BOARD)-$(MIDDLEWAREVERSION)
+	@cp -r /builder/deb/sensor-config/* $(BUILDDIR)/package/sensor-config-$(BOARD)-$(MIDDLEWAREVERSION)/
+	@mkdir -pv /$(BUILDDIR)/package/sensor-config-$(BOARD)-$(MIDDLEWAREVERSION)/mnt/cfg/param/
+	@rsync -avpPxH addons/device-config/overlay/mnt/cfg/param/ /$(BUILDDIR)/package/sensor-config-$(BOARD)-$(MIDDLEWAREVERSION)/mnt/cfg/param/
+	@rm -f /$(BUILDDIR)/package/sensor-config-$(BOARD)-$(MIDDLEWAREVERSION)/mnt/cfg/param/cvi_sdr_bin
+	@mkdir -pv /$(BUILDDIR)/package/sensor-config-$(BOARD)-$(MIDDLEWAREVERSION)/mnt/data/
+	@rsync -avpPxH addons/device-config/overlay/mnt/data/ /$(BUILDDIR)/package/sensor-config-$(BOARD)-$(MIDDLEWAREVERSION)/mnt/data/
+	@rm -f /$(BUILDDIR)/package/sensor-config-$(BOARD)-$(MIDDLEWAREVERSION)/mnt/data/sensor_cfg.ini
+	@sed -i 's/Version: 1.0.0/Version: $(MIDDLEWAREVERSION)/' $(BUILDDIR)/package/sensor-config-$(BOARD)-$(MIDDLEWAREVERSION)/DEBIAN/control
+	@sed -i 's/Package: sensor-config/Package: sensor-config-$(BOARD)/' $(BUILDDIR)/package/sensor-config-$(BOARD)-$(MIDDLEWAREVERSION)/DEBIAN/control
+	@cd $(BUILDDIR)/package/ && dpkg-deb --build sensor-config-$(BOARD)-$(MIDDLEWAREVERSION) sensor-config-$(BOARD)_$(MIDDLEWAREVERSION)_$(DEB_ARCH).deb
+	@cp $(BUILDDIR)/package/sensor-config-$(BOARD)_$(MIDDLEWAREVERSION)_$(DEB_ARCH).deb /output/
+	@mkdir -p /rootfs/tmp/install/
+	@cp /output/sensor-config-*.deb /rootfs/tmp/install/
+	@touch $@
+
+sensor-config: $(BUILDDIR)/sensor-config-install-stamp $(BUILDDIR)/sensor-config-package-stamp
+
+$(BUILDDIR)/device-config-stamp: firmware-vcodec sensor-config
 	@echo "$(COLOUR_GREEN)Installing device-config for $(BOARD)$(END_COLOUR)"
 	@mkdir -pv /rootfs/etc/init.d/
 	@cp -a addons/device-config/S02config /rootfs/etc/init.d/
 	@chmod +x /rootfs/etc/init.d/S02config
 	@cp -a addons/device-config/device-config*.service /rootfs/etc/systemd/system/
-	@mkdir -pv /rootfs/mnt/
-	@rsync -avpPxH addons/device-config/overlay/mnt/ /rootfs/mnt/
 	@mkdir -p /rootfs/tmp/install/
 	@echo " device-config" >> /rootfs/tmp/install/systemd-enable
 	@touch $@
