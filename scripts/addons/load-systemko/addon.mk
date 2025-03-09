9405f3dbcb076373a830dcb26435a1d389e45a30
scpcom <scpcom@gmx.de>
2025-03-09 21:59:16 +0100

    package load-systemko

--- scripts/addons/load-systemko/addon.mk
+++ scripts/addons/load-systemko/addon.mk
@@ -1,21 +1,31 @@
 $(BUILDDIR)/load-systemko-stamp:
+	@echo "$(COLOUR_GREEN)Packaging load-systemko for $(BOARD)$(END_COLOUR)"
+	@mkdir -p $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)
+	@cp -r /builder/deb/load-systemko/* $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/
+	@mkdir -pv $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/init.d/
+	@cp -a addons/load-systemko/S00kmod $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/init.d/
+	@chmod +x $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/init.d/S00kmod
+	@cp -a addons/load-systemko/S04backlight $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/init.d/
+	@chmod +x $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/init.d/S04backlight
+	@cp -a addons/load-systemko/S04fb $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/init.d/
+	@chmod +x $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/init.d/S04fb
+	@cp -a addons/load-systemko/S05tp $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/init.d/
+	@chmod +x $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/init.d/S05tp
+	@cp -a addons/load-systemko/S25wifimod $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/init.d/
+	@chmod +x $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/init.d/S25wifimod
+	@mkdir -pv $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/systemd/system/
+	@cp -a addons/load-systemko/load-systemko*.service $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/systemd/system/
+	@cp -a addons/load-systemko/enable-backlight*.service $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/systemd/system/
+	@cp -a addons/load-systemko/load-fb*.service $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/systemd/system/
+	@cp -a addons/load-systemko/load-tp*.service $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/systemd/system/
+	@cp -a addons/load-systemko/load-wifimod*.service $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/etc/systemd/system/
+	@sed -i 's/Version: 1.0.0/Version: $(OSDRVVERSION)/' $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/DEBIAN/control
+	@sed -i 's/Package: load-systemko/Package: load-systemko-$(BOARD)/' $(BUILDDIR)/package/load-systemko-$(BOARD)-$(OSDRVVERSION)/DEBIAN/control
+	@cd $(BUILDDIR)/package/ && dpkg-deb --build load-systemko-$(BOARD)-$(OSDRVVERSION) load-systemko-$(BOARD)_$(OSDRVVERSION)_$(DEB_ARCH).deb
+	@cp $(BUILDDIR)/package/load-systemko-$(BOARD)_$(OSDRVVERSION)_$(DEB_ARCH).deb /output/
+	@mkdir -p /rootfs/tmp/install/
+	@cp /output/load-systemko-*.deb /rootfs/tmp/install/
 	@echo "$(COLOUR_GREEN)Installing load-systemko for $(BOARD)$(END_COLOUR)"
-	@mkdir -pv /rootfs/etc/init.d/
-	@cp -a addons/load-systemko/S00kmod /rootfs/etc/init.d/
-	@chmod +x /rootfs/etc/init.d/S00kmod
-	@cp -a addons/load-systemko/S04backlight /rootfs/etc/init.d/
-	@chmod +x /rootfs/etc/init.d/S04backlight
-	@cp -a addons/load-systemko/S04fb /rootfs/etc/init.d/
-	@chmod +x /rootfs/etc/init.d/S04fb
-	@cp -a addons/load-systemko/S05tp /rootfs/etc/init.d/
-	@chmod +x /rootfs/etc/init.d/S05tp
-	@cp -a addons/load-systemko/S25wifimod /rootfs/etc/init.d/
-	@chmod +x /rootfs/etc/init.d/S25wifimod
-	@cp -a addons/load-systemko/load-systemko*.service /rootfs/etc/systemd/system/
-	@cp -a addons/load-systemko/enable-backlight*.service /rootfs/etc/systemd/system/
-	@cp -a addons/load-systemko/load-fb*.service /rootfs/etc/systemd/system/
-	@cp -a addons/load-systemko/load-tp*.service /rootfs/etc/systemd/system/
-	@cp -a addons/load-systemko/load-wifimod*.service /rootfs/etc/systemd/system/
 	@mkdir -p /rootfs/tmp/install/
 	@echo " load-systemko" >> /rootfs/tmp/install/systemd-enable
 	@echo " enable-backlight" >> /rootfs/tmp/install/systemd-enable
