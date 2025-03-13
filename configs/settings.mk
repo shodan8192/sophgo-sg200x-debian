5eaba0c50731d016dcd4bbe3a90d9b471d4be12f
scpcom <scpcom@gmx.de>
2025-03-13 02:34:30 +0100

    add python3-requests and wget to target image

--- configs/settings.mk
+++ configs/settings.mk
@@ -2,7 +2,7 @@ KERNELREV="6"
 FSBLVERSION=1.1.0
 OSDRVVERSION=2024.10.14
 MIDDLEWAREVERSION=2024.10.14
-PACKAGES="busybox-static ca-certificates debian-archive-keyring dosfstools binutils file tree sudo bash-completion u-boot-menu openssh-server dnsmasq-base libpam-systemd ppp libatomic1 libgomp1 libengine-pkcs11-openssl iptables lldpd psmisc systemd-timesyncd vim usbutils parted exfatprogs systemd-sysv i2c-tools net-tools ifupdown ethtool avahi-utils sudo gnupg rsync gpiod u-boot-tools libubootenv-tool unzip"
+PACKAGES="busybox-static ca-certificates debian-archive-keyring dosfstools binutils file tree sudo bash-completion u-boot-menu openssh-server dnsmasq-base libpam-systemd ppp libatomic1 libgomp1 libengine-pkcs11-openssl iptables lldpd psmisc systemd-timesyncd vim usbutils parted exfatprogs systemd-sysv i2c-tools net-tools ifupdown ethtool avahi-utils sudo gnupg rsync gpiod u-boot-tools libubootenv-tool python3-requests unzip wget"
 ifeq ("$(findstring kvm,$(VARIANT))","")
 PACKAGES += " network-manager"
 endif
