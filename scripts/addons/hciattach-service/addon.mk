1e70bee4f7c8097d2b293d6230e930580bd7152a
scpcom <scpcom@gmx.de>
2025-03-14 20:42:23 +0100

    package hciattach-uart

--- scripts/addons/hciattach-service/addon.mk
+++ scripts/addons/hciattach-service/addon.mk
@@ -1,7 +1,17 @@
 $(BUILDDIR)/hciattach-service-stamp:
+	@echo "$(COLOUR_GREEN)Packaging hciattach systemd service for $(BOARD)$(END_COLOUR)"
+	@$(eval HCIATTACHVERSION=$(shell echo "1.0.0"))
+	@mkdir -p $(BUILDDIR)/package/hciattach-uart-$(BOARD)-$(HCIATTACHVERSION)
+	@cp -r /builder/deb/hciattach-uart/* $(BUILDDIR)/package/hciattach-uart-$(BOARD)-$(HCIATTACHVERSION)/
+	@mkdir -p $(BUILDDIR)/package/hciattach-uart-$(BOARD)-$(HCIATTACHVERSION)/etc/systemd/system/
+	@cp -a addons/hciattach-service/hciattach.service $(BUILDDIR)/package/hciattach-uart-$(BOARD)-$(HCIATTACHVERSION)/etc/systemd/system/
+	@sed -i 's/Version: 1.0.0/Version: $(HCIATTACHVERSION)/' $(BUILDDIR)/package/hciattach-uart-$(BOARD)-$(HCIATTACHVERSION)/DEBIAN/control
+	@sed -i 's/Package: hciattach-uart/Package: hciattach-uart-$(BOARD)/' $(BUILDDIR)/package/hciattach-uart-$(BOARD)-$(HCIATTACHVERSION)/DEBIAN/control
+	@cd $(BUILDDIR)/package/ && dpkg-deb --build hciattach-uart-$(BOARD)-$(HCIATTACHVERSION) hciattach-uart-$(BOARD)_$(HCIATTACHVERSION)_$(DEB_ARCH).deb
+	@cp $(BUILDDIR)/package/hciattach-uart-$(BOARD)_$(HCIATTACHVERSION)_$(DEB_ARCH).deb /output/
+	@mkdir -p /rootfs/tmp/install/
+	@cp /output/hciattach-uart-*.deb /rootfs/tmp/install/
 	@echo "$(COLOUR_GREEN)Installing hciattach systemd service for $(BOARD)$(END_COLOUR)"
-	@mkdir -p /rootfs/etc/systemd/system/ 
-	@cp -a addons/hciattach-service/hciattach.service /rootfs/etc/systemd/system/ 
 	@mkdir -p /rootfs/tmp/install/
 	@echo " hciattach bluetooth" >> /rootfs/tmp/install/systemd-enable
 	@touch $@
