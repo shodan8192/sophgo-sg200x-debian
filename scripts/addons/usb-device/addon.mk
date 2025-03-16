96c73d6bbf292d56f2de01466ed2c5b6d7cdd31b
scpcom <scpcom@gmx.de>
2025-03-16 19:12:51 +0100

    addons: usb-device: add variant to package if needed

--- scripts/addons/usb-device/addon.mk
+++ scripts/addons/usb-device/addon.mk
@@ -1,22 +1,34 @@
+ifneq ("$(findstring usb-device,$(IMAGE_ADDITIONS))","")
+BSPDEPENDS += usb-device-$(BOARD_EXT)
+BSPFILTER += "usb-device"
+endif
+
 $(BUILDDIR)/usb-device-stamp: $(BUILDDIR)/buildroot-prepare-checkout-stamp
 	@echo "$(COLOUR_GREEN)Packaging usb-device for $(BOARD)$(END_COLOUR)"
 	@$(eval USBDEVVERSION=$(shell echo "1.0.0"))
 	@$(eval BV=$(shell cd $(BUILDDIR)/buildroot && git log -1 --format="%at" | xargs -I{} date -d @{} +-%Y%m%d-${KERNELREV}))
-	@mkdir -p $(BUILDDIR)/package/usb-device-$(BOARD)-$(USBDEVVERSION)
-	@cp -r /builder/deb/usb-device/* $(BUILDDIR)/package/usb-device-$(BOARD)-$(USBDEVVERSION)/
-	@mkdir -pv $(BUILDDIR)/package/usb-device-$(BOARD)-$(USBDEVVERSION)/etc/init.d/
+	@mkdir -p $(BUILDDIR)/package/usb-device-$(BOARD_EXT)-$(USBDEVVERSION)
+	@cp -r /builder/deb/usb-device/* $(BUILDDIR)/package/usb-device-$(BOARD_EXT)-$(USBDEVVERSION)/
+	@mkdir -pv $(BUILDDIR)/package/usb-device-$(BOARD_EXT)-$(USBDEVVERSION)/etc/init.d/
 	@if [ "X$(findstring kvm,$(VARIANT))" = "X" ]; then \
-		cp -a addons/usb-device/S03usbdev $(BUILDDIR)/package/usb-device-$(BOARD)-$(USBDEVVERSION)/etc/init.d/ ; \
+		cp -a addons/usb-device/S03usbdev $(BUILDDIR)/package/usb-device-$(BOARD_EXT)-$(USBDEVVERSION)/etc/init.d/ ; \
 	else \
-		cp -a addons/nanokvm/S03usbdev $(BUILDDIR)/package/usb-device-$(BOARD)-$(USBDEVVERSION)/etc/init.d/ ; \
+		cp -a addons/nanokvm/S03usbdev $(BUILDDIR)/package/usb-device-$(BOARD_EXT)-$(USBDEVVERSION)/etc/init.d/ ; \
 	fi
-	@chmod +x $(BUILDDIR)/package/usb-device-$(BOARD)-$(USBDEVVERSION)/etc/init.d/S03usbdev
-	@mkdir -pv $(BUILDDIR)/package/usb-device-$(BOARD)-$(USBDEVVERSION)/etc/systemd/system/
-	@cp -a addons/usb-device/usb-device*.service $(BUILDDIR)/package/usb-device-$(BOARD)-$(USBDEVVERSION)/etc/systemd/system/
-	@sed -i 's/Version: 1.0.0/Version: $(USBDEVVERSION)$(BV)/' $(BUILDDIR)/package/usb-device-$(BOARD)-$(USBDEVVERSION)/DEBIAN/control
-	@sed -i 's/Package: usb-device/Package: usb-device-$(BOARD)/' $(BUILDDIR)/package/usb-device-$(BOARD)-$(USBDEVVERSION)/DEBIAN/control
-	@cd $(BUILDDIR)/package/ && dpkg-deb --build usb-device-$(BOARD)-$(USBDEVVERSION) usb-device-$(BOARD)_$(USBDEVVERSION)$(BV)_$(DEB_ARCH).deb
-	@cp $(BUILDDIR)/package/usb-device-$(BOARD)_$(USBDEVVERSION)$(BV)_$(DEB_ARCH).deb /output/
+	@chmod +x $(BUILDDIR)/package/usb-device-$(BOARD_EXT)-$(USBDEVVERSION)/etc/init.d/S03usbdev
+	@mkdir -pv $(BUILDDIR)/package/usb-device-$(BOARD_EXT)-$(USBDEVVERSION)/etc/systemd/system/
+	@cp -a addons/usb-device/usb-device*.service $(BUILDDIR)/package/usb-device-$(BOARD_EXT)-$(USBDEVVERSION)/etc/systemd/system/
+	@sed -i 's/Version: 1.0.0/Version: $(USBDEVVERSION)$(BV)/' $(BUILDDIR)/package/usb-device-$(BOARD_EXT)-$(USBDEVVERSION)/DEBIAN/control
+	@sed -i 's/Package: usb-device/Package: usb-device-$(BOARD_EXT)/' $(BUILDDIR)/package/usb-device-$(BOARD_EXT)-$(USBDEVVERSION)/DEBIAN/control
+	@if [ "$(BOARD)" = "$(BOARD_EXT)" ]; then \
+		sed -i '/Provides: .*/d' $(BUILDDIR)/package/usb-device-$(BOARD_EXT)-$(USBDEVVERSION)/DEBIAN/control && \
+		sed -i '/Replaces: .*/d' $(BUILDDIR)/package/usb-device-$(BOARD_EXT)-$(USBDEVVERSION)/DEBIAN/control ; \
+	else \
+		sed -i 's/Provides: .*/Provides: usb-device-$(BOARD)/' $(BUILDDIR)/package/usb-device-$(BOARD_EXT)-$(USBDEVVERSION)/DEBIAN/control && \
+		sed -i 's/Replaces: .*/Replaces: usb-device-$(BOARD)/' $(BUILDDIR)/package/usb-device-$(BOARD_EXT)-$(USBDEVVERSION)/DEBIAN/control ; \
+	fi
+	@cd $(BUILDDIR)/package/ && dpkg-deb --build usb-device-$(BOARD_EXT)-$(USBDEVVERSION) usb-device-$(BOARD_EXT)_$(USBDEVVERSION)$(BV)_$(DEB_ARCH).deb
+	@cp $(BUILDDIR)/package/usb-device-$(BOARD_EXT)_$(USBDEVVERSION)$(BV)_$(DEB_ARCH).deb /output/
 	@mkdir -p /rootfs/tmp/install/
 	@cp /output/usb-device-*.deb /rootfs/tmp/install/
 	@echo "$(COLOUR_GREEN)Installing usb-device for $(BOARD)$(END_COLOUR)"
