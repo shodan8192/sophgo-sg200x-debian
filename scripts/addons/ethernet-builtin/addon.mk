bfc0dc4e90aeeb8112d8b5217586f20c6411048b
scpcom <scpcom@gmx.de>
2025-03-16 16:34:04 +0100

    addons: ethernet-builtin: add to bsp filter

--- scripts/addons/ethernet-builtin/addon.mk
+++ scripts/addons/ethernet-builtin/addon.mk
@@ -1,3 +1,7 @@
+ifneq ("$(findstring ethernet-builtin,$(IMAGE_ADDITIONS))","")
+BSPFILTER += "ethernet-builtin"
+endif
+
 $(BUILDDIR)/ethernet-builtin-stamp:
 	@echo "$(COLOUR_GREEN)Installing ethernet-builtin for $(BOARD)$(END_COLOUR)"
 	@mkdir -p /rootfs/etc/network/interfaces.d/
