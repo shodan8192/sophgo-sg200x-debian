32fb3fd47b360390ef83a32178660c4f28ba9d54
scpcom <scpcom@gmx.de>
2025-03-23 18:48:03 +0100

    remove duplicate sudo from packages

--- configs/settings.mk
+++ configs/settings.mk
@@ -3,7 +3,7 @@ FSBLVERSION=1.1.0
 BSPVERSION=1.0.30
 OSDRVVERSION=2024.10.14
 MIDDLEWAREVERSION=2024.10.14
-PACKAGES="busybox-static ca-certificates debian-archive-keyring dosfstools binutils file tree sudo bash-completion u-boot-menu openssh-server dnsmasq-base libpam-systemd ppp libatomic1 libgomp1 libengine-pkcs11-openssl iptables lldpd psmisc systemd-timesyncd vim usbutils parted exfatprogs systemd-sysv i2c-tools net-tools ifupdown ethtool avahi-utils sudo gnupg rsync gpiod u-boot-tools libubootenv-tool python3-requests unzip wget"
+PACKAGES="busybox-static ca-certificates debian-archive-keyring dosfstools binutils file tree sudo bash-completion u-boot-menu openssh-server dnsmasq-base libpam-systemd ppp libatomic1 libgomp1 libengine-pkcs11-openssl iptables lldpd psmisc systemd-timesyncd vim usbutils parted exfatprogs systemd-sysv i2c-tools net-tools ifupdown ethtool avahi-utils gnupg rsync gpiod u-boot-tools libubootenv-tool python3-requests unzip wget"
 ifeq ("$(findstring kvm,$(VARIANT))","")
 PACKAGES += " network-manager"
 endif
