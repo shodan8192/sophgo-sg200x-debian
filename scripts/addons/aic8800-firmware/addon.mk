e38050c829ef65d9e9ba3312c019b01f2c2cf17f
scpcom <scpcom@gmx.de>
2026-01-18 00:36:33 +0100

    addons: aic8800-firmware: update for axera

--- scripts/addons/aic8800-firmware/addon.mk
+++ scripts/addons/aic8800-firmware/addon.mk
@@ -3,11 +3,12 @@ BSPDEPENDS += firmware-aic8800-$(CHIP)
 BSPFILTER += "aic8800-firmware"
 endif
 
+AIC8800_TARGET_DIR ?= /lib/firmware/aic8800_sdio
+
 $(BUILDDIR)/aic8800-firmware-stamp:
 	@echo "$(COLOUR_GREEN)Installing aic8800-firmware for $(BOARD)$(END_COLOUR)"
 	@$(eval AIC8800RELEASE=$(shell echo "2"))
 	@$(eval AIC8800_PACKAGE_DIR=$(BUILDDIR)/package/firmware-aic8800-$(CHIP)-$(OSDRVVERSION))
-	@$(eval AIC8800_TARGET_DIR=/lib/firmware/aic8800_sdio)
 	@rm -rf $(BUILDDIR)/aic8800-firmware
 	@git clone --depth 1 https://github.com/armbian/firmware.git $(BUILDDIR)/aic8800-firmware
 	@git clone --depth 1 https://github.com/scpcom/aic8800-sdio-firmware $(BUILDDIR)/aic8800-sdio-firmware
@@ -23,6 +24,10 @@ $(BUILDDIR)/aic8800-firmware-stamp:
 	@sed -i 's/Architecture: riscv64/Architecture: $(DEB_ARCH)/' $(AIC8800_PACKAGE_DIR)/DEBIAN/control
 	@sed -i 's/Version: 1.0.0/Version: $(OSDRVVERSION)-$(AIC8800RELEASE)/' $(AIC8800_PACKAGE_DIR)/DEBIAN/control
 	@sed -i 's/Package: firmware-aic8800-cv181x/Package: firmware-aic8800-$(CHIP)/' $(AIC8800_PACKAGE_DIR)/DEBIAN/control
+	@sed -i 's/CVITEK/$(CHIP_VENDOR)/' $(AIC8800_PACKAGE_DIR)/DEBIAN/control
+	@sed -i 's/CV18xx and SG200X/$(CHIP)/' $(AIC8800_PACKAGE_DIR)/DEBIAN/control
+	@sed -i 's/cv181x/$(CHIP)/' $(AIC8800_PACKAGE_DIR)/DEBIAN/control
+	@sed -i 's/RISC-V/$(ARCH_NAME)/' $(AIC8800_PACKAGE_DIR)/DEBIAN/control
 	@cd $(BUILDDIR)/package/ && dpkg-deb --build firmware-aic8800-$(CHIP)-$(OSDRVVERSION) firmware-aic8800-$(CHIP)_$(OSDRVVERSION)-$(AIC8800RELEASE)_$(DEB_ARCH).deb
 	@cp $(BUILDDIR)/package/firmware-aic8800-$(CHIP)_$(OSDRVVERSION)-$(AIC8800RELEASE)_$(DEB_ARCH).deb /output/
 	@mkdir -p /rootfs/tmp/install/
