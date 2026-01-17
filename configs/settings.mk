e9f820cdc181c6288c95ab40550326328a2cb5a7
scpcom <scpcom@gmx.de>
2026-01-17 22:53:16 +0100

    move gpiod to chip

--- configs/settings.mk
+++ configs/settings.mk
@@ -1,6 +1,6 @@
 KERNELREV="6"
 BSPVERSION=1.0.60
-PACKAGES="busybox-static ca-certificates debian-archive-keyring dosfstools binutils file tree sudo bash-completion u-boot-menu openssh-server dnsmasq-base libpam-systemd ppp libatomic1 libgomp1 libengine-pkcs11-openssl iptables lldpd locales locales-all picocom psmisc systemd-timesyncd vim usbutils parted exfatprogs systemd-sysv i2c-tools net-tools ifupdown ethtool avahi-utils gnupg rsync gpiod u-boot-tools libubootenv-tool bc fake-hwclock lzip python3-requests unzip wget"
+PACKAGES="busybox-static ca-certificates debian-archive-keyring dosfstools binutils file tree sudo bash-completion u-boot-menu openssh-server dnsmasq-base libpam-systemd ppp libatomic1 libgomp1 libengine-pkcs11-openssl iptables lldpd locales locales-all picocom psmisc systemd-timesyncd vim usbutils parted exfatprogs systemd-sysv i2c-tools net-tools ifupdown ethtool avahi-utils gnupg rsync u-boot-tools libubootenv-tool bc fake-hwclock lzip python3-requests unzip wget"
 ifeq ("$(findstring kvm,$(VARIANT))","")
 PACKAGES += " network-manager"
 endif
