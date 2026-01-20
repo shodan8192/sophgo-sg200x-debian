adc928dcf04a6adb5cbafbd0accf51823885b5fa
scpcom <scpcom@gmx.de>
2026-01-20 01:43:12 +0100

    nanokvmpro: update package list for jammy

--- configs/nanokvmpro/settings.mk
+++ configs/nanokvmpro/settings.mk
@@ -14,7 +14,13 @@ VARIANT?=kvm
 
 PACKAGES += " hostapd udhcpd wireless-regdb wpasupplicant"
 PACKAGES += " libasound2-dev libcjson-dev libconfig-dev libdrm-dev libevent-dev libjpeg-dev libjson-c5 libnice-dev libopus-dev libspeexdsp-dev libsrtp2-dev libwebsockets-dev libxkbcommon-dev nginx"
-PACKAGES += " python3-aiofiles python3-aiohttp python3-async-lru python3-dbus-next python3-evdev python3-mako python3-netifaces python3-passlib python3-pil python3-psutil python3-pyghmi python3-pygments python3-pyotp python3-serial python3-setproctitle python3-systemd python3-xlib python3-yaml python3-zstandard python-is-python3 tesseract-ocr"
+PACKAGES += " python3-aiofiles python3-aiohttp python3-evdev python3-mako python3-netifaces python3-passlib python3-pil python3-psutil python3-pyghmi python3-pygments python3-pyotp python3-serial python3-setproctitle python3-systemd python3-xlib python3-yaml python-is-python3 tesseract-ocr"
+ifneq ("$(DEB_DISTRO)","jammy")
+PACKAGES += " python3-async-lru python3-dbus-next python3-zstandard"
+endif
+ifneq ("$(findstring ubuntu,$(DEB_URL))","")
+PACKAGES += " ttyd"
+endif
 
 #IMAGE_ADDITIONS += "sensor-config"
 IMAGE_ADDITIONS += "device-key"
