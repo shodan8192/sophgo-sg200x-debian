625649af8e61757be77f58fb5b0d229016b5bb54
scpcom <scpcom@gmx.de>
2026-01-22 01:48:36 +0100

    nanokvmpro: use maixcam2-python3 if required

--- configs/nanokvmpro/settings.mk
+++ configs/nanokvmpro/settings.mk
@@ -13,10 +13,12 @@ STORAGE_TYPE=emmc
 VARIANT?=kvm
 
 PACKAGES += " hostapd udhcpd wireless-regdb wpasupplicant"
-PACKAGES += " libasound2-dev libcjson-dev libconfig-dev libdrm-dev libevent-dev libjpeg-dev libjson-c5 libnice-dev libopus-dev libspeexdsp-dev libsrtp2-dev libwebsockets-dev libxkbcommon-dev nginx"
-PACKAGES += " python3-aiofiles python3-aiohttp python3-evdev python3-mako python3-netifaces python3-passlib python3-pil python3-psutil python3-pyghmi python3-pygments python3-pyotp python3-serial python3-setproctitle python3-systemd python3-xlib python3-yaml python-is-python3 tesseract-ocr"
-ifneq ("$(DEB_DISTRO)","jammy")
+PACKAGES += " build-essential libasound2-dev libcjson-dev libconfig-dev libdbus-1-dev libdrm-dev libevent-dev libjpeg-dev libjson-c5 libnice-dev libopus-dev libspeexdsp-dev libsrtp2-dev libsystemd-dev libwebsockets-dev libxkbcommon-dev nginx tesseract-ocr xz-utils"
+ifeq ("$(DEB_DISTRO)","trixie")
+PACKAGES += " python3-aiofiles python3-aiohttp python3-evdev python3-mako python3-netifaces python3-passlib python3-pil python3-psutil python3-pyghmi python3-pygments python3-pyotp python3-serial python3-setproctitle python3-systemd python3-xlib python3-yaml python-is-python3"
 PACKAGES += " python3-async-lru python3-dbus-next python3-zstandard"
+else
+IMAGE_ADDITIONS += "maixcam2-python3"
 endif
 ifneq ("$(findstring ubuntu,$(DEB_URL))","")
 PACKAGES += " ttyd"
