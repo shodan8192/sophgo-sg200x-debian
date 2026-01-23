ebd93751905bc15839ae3e633fd140630760fe31
scpcom <scpcom@gmx.de>
2026-01-23 01:11:14 +0100

    distro: set parameters based on DEB_DISTRO

--- configs/distro.mk
+++ configs/distro.mk
@@ -1,4 +1,12 @@
-DEB_URL ?= http://ports.ubuntu.com/ubuntu-ports
 DEB_DISTRO ?= noble
+DEB_KEYSERVER ?= keyserver.ubuntu.com
+ifneq ($(findstring "$(DEB_DISTRO)","bullseye" "bookworm" "trixie"),)
+DEB_URL ?= http://deb.debian.org/debian
+DEB_COMPONENTS ?= main
+DEB_COMPONENTS_FULL ?= main non-free-firmware
+DEB_PUBKEY ?= 78DBA3BC47EF2265
+else
+DEB_URL ?= http://ports.ubuntu.com/ubuntu-ports
 DEB_COMPONENTS ?= main universe
 DEB_COMPONENTS_FULL ?= main restricted universe multiverse
+endif
