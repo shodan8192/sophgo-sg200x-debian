38d2af7db34d2a92ae4c37f30c0cea40aa0d2fe6
scpcom <scpcom@gmx.de>
2026-01-23 18:37:24 +0100

    chip: ax620e: bsp: update 2352e03

--- configs/chip/ax620e/chip.mk
+++ configs/chip/ax620e/chip.mk
@@ -396,7 +396,7 @@ $(BUILDDIR)/bsp-prepare-checkout-stamp:
 	@echo "$(COLOUR_GREEN)Checking out BSP for $(BOARD)$(END_COLOUR)"
 	@mkdir -p $(BUILDDIR)
 	@git clone -b main $(GIT_CLONE_OPTS) --recursive https://github.com/scpcom/ax620e-bsp-build $(BUILDDIR)/bsp
-	@cd $(BUILDDIR)/bsp && git checkout 2417c4b
+	@cd $(BUILDDIR)/bsp && git checkout 2352e03
 	@touch $@
 
 $(BUILDDIR)/bsp-prepare-patch-stamp: $(BUILDDIR)/toolchain-prepare-patch-stamp $(BUILDDIR)/bsp-prepare-checkout-stamp
