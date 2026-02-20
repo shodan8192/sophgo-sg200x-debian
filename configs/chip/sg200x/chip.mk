dd15bf0b2e9b9fef448c0a6e4407522482f768f3
scpcom <scpcom@gmx.de>
2026-02-21 00:07:45 +0100

    u-boot: update 3790a21

--- configs/chip/sg200x/chip.mk
+++ configs/chip/sg200x/chip.mk
@@ -556,7 +556,7 @@ $(BUILDDIR)/uboot-prepare-checkout-stamp:
 	@echo "$(COLOUR_GREEN)Checking out U-Boot for $(BOARD)$(END_COLOUR)"
 	@mkdir -p $(BUILDDIR)
 	@git clone -b licheervnano-cvisdk-2021.10 $(GIT_CLONE_OPTS) $(GIT_USER_URL)/u-boot $(BUILDDIR)/u-boot
-	@cd $(BUILDDIR)/u-boot && git checkout 0963ce4
+	@cd $(BUILDDIR)/u-boot && git checkout 3790a21
 	@touch $@
 
 $(BUILDDIR)/uboot-prepare-patch-stamp: $(BUILDDIR)/toolchain-prepare-patch-stamp $(BUILDDIR)/uboot-prepare-checkout-stamp $(BUILDDIR)/$(BOARD)-$(VARIANT)/cvi_board_memmap.h
