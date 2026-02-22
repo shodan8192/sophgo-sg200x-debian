a6b6fb1fe3af2d82e876d636ffcca18347522229
scpcom <scpcom@gmx.de>
2026-02-22 21:28:42 +0100

    bsp: update a107cc2

--- configs/chip/ax620e/chip.mk
+++ configs/chip/ax620e/chip.mk
@@ -449,7 +449,7 @@ $(BUILDDIR)/bsp-prepare-clone-stamp:
 
 $(BUILDDIR)/bsp-prepare-checkout-stamp: $(BUILDDIR)/bsp-prepare-clone-stamp
 	@echo "$(COLOUR_GREEN)Checking out BSP for $(BOARD)$(END_COLOUR)"
-	@cd $(BUILDDIR)/bsp && git checkout a236692
+	@cd $(BUILDDIR)/bsp && git checkout a107cc2
 	@cd $(BUILDDIR)/bsp && git submodule set-url axerabin $(GIT_USER_URL)/axerabin
 	@cd $(BUILDDIR)/bsp && git submodule set-url linux $(GIT_USER_URL)/linux
 	@cd $(BUILDDIR)/bsp && git submodule set-url u-boot $(GIT_USER_URL)/u-boot
