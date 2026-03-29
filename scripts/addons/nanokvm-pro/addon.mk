b4ea5a4012e01ff3331f5be3ae7250f5280fff39
scpcom <scpcom@gmx.de>
2026-03-29 03:22:24 +0200

    addons: nanokvm-pro: remove obsolete debs

--- scripts/addons/nanokvm-pro/addon.mk
+++ scripts/addons/nanokvm-pro/addon.mk
@@ -157,24 +157,8 @@ $(BUILDDIR)/nanokvm-pro-prepare-stamp: $(BUILDDIR)/golang-toolchain-stamp $(BUIL
 	@cp -p $(BUILDDIR)/nanokvm-pro/kvmadmin.tar.gz /output/$(BOARD)-kvmadmin.tar.gz
 	@touch $@
 
-ifeq ($(NANOKVM_PRO_DEBS_FROM_SOURCE),y)
-$(BUILDDIR)/nanokvm-pro-debs-stamp: $(BUILDDIR)/libconfig-stamp $(BUILDDIR)/libjpeg-turbo-stamp $(BUILDDIR)/libwebsockets-stamp $(BUILDDIR)/opus-stamp $(BUILDDIR)/ttyd-stamp
+$(BUILDDIR)/nanokvm-pro-debs-stamp: $(BUILDDIR)/nanokvm-pro-prepare-stamp $(BUILDDIR)/opus-stamp $(BUILDDIR)/ttyd-stamp
 	@touch $@
-else
-$(BUILDDIR)/nanokvm-pro-debs-stamp: $(BUILDDIR)/nanokvm-pro-prepare-stamp
-	@mkdir -p $(BUILDDIR)/nanokvm-pro/debs
-	@cd $(BUILDDIR)/nanokvm-pro/debs ; [ "$(findstring ubuntu,$(DEB_URL))" != "" ] || wget -N https://launchpadlibrarian.net/587202705/libjpeg-turbo8_2.1.2-0ubuntu1_arm64.deb || wget -N $(USER_SITE_URL)/deb/pool/$(CHIP_FAMILY)/libjpeg-turbo8_2.1.2-0ubuntu1_$(DEB_ARCH).deb
-	@cd $(BUILDDIR)/nanokvm-pro/debs ; [ "$(DEB_DISTRO)" != "trixie" ] || wget -N https://launchpadlibrarian.net/470183065/libconfig9_1.5-0.4build1_arm64.deb || wget -N $(USER_SITE_URL)/deb/pool/$(CHIP_FAMILY)/libconfig9_1.5-0.4build1_$(DEB_ARCH).deb
-	@cd /output/ ; [ "$(DEB_DISTRO)" != "trixie" ] || wget -N https://launchpadlibrarian.net/470183062/libconfig-dev_1.5-0.4build1_arm64.deb || wget -N $(USER_SITE_URL)/deb/pool/$(CHIP_FAMILY)/libconfig-dev_1.5-0.4build1_$(DEB_ARCH).deb
-	@cd /output/ ; wget -N https://launchpadlibrarian.net/592830919/libopus0_1.3.1-0.1build2_arm64.deb || wget -N $(USER_SITE_URL)/deb/pool/$(CHIP_FAMILY)/libopus0_1.3.1-0.1build2_$(DEB_ARCH).deb
-	@cd /output/ ; wget -N https://launchpadlibrarian.net/592830918/libopus-dev_1.3.1-0.1build2_arm64.deb || wget -N $(USER_SITE_URL)/deb/pool/$(CHIP_FAMILY)/libopus-dev_1.3.1-0.1build2_$(DEB_ARCH).deb
-	@cd $(BUILDDIR)/nanokvm-pro/debs ; [ "$(DEB_DISTRO)" = "jammy" ] || wget -N https://launchpadlibrarian.net/571748137/libwebsockets16_4.0.20-2ubuntu1_arm64.deb || wget -N $(USER_SITE_URL)/deb/pool/$(CHIP_FAMILY)/libwebsockets16_4.0.20-2ubuntu1_$(DEB_ARCH).deb
-	@cd $(BUILDDIR)/nanokvm-pro/debs ; [ "$(findstring ubuntu,$(DEB_URL))" != "" ] || wget -N https://launchpadlibrarian.net/572052652/ttyd_1.6.3+20210924-1build1_arm64.deb || wget -N $(USER_SITE_URL)/deb/pool/$(CHIP_FAMILY)/ttyd_1.6.3+20210924-1build1_$(DEB_ARCH).deb
-	@[ "$(DEB_DISTRO)" = "jammy" ] || cp -p $(BUILDDIR)/nanokvm-pro/debs/*.deb /output/
-	@mkdir -p /rootfs/tmp/install/
-	@[ "$(DEB_DISTRO)" = "jammy" ] || cp -p $(BUILDDIR)/nanokvm-pro/debs/*.deb /rootfs/tmp/install/
-	@touch $@
-endif
 
 $(BUILDDIR)/nanokvm-pro-package-prepare-stamp: $(BUILDDIR)/nanokvm-pro-prepare-stamp $(BUILDDIR)/nanokvm-pro-debs-stamp
 	@cd $(BUILDDIR)/nanokvm-pro ; dpkg-deb -R nanokvm_pro_$(NANOKVM_PRO_VERSION)/nanokvmpro_$(NANOKVM_PRO_VERSION)_$(DEB_ARCH).deb $(NANOKVM_PRO_PACKAGE_DIR)
