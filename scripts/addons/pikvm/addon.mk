9de1e84110ac5f98ca6651b0f80fee9e0c220331
scpcom <scpcom@gmx.de>
2026-04-07 23:27:40 +0200

    addons: pikvm: create /rootfs/tmp/install if missing

--- scripts/addons/pikvm/addon.mk
+++ scripts/addons/pikvm/addon.mk
@@ -36,6 +36,7 @@ $(BUILDDIR)/pikvm-stamp: $(BUILDDIR)/middleware-package-stamp $(BUILDDIR)/pikvm-
 	@mkdir -pv /rootfs/kvmapp/server/dl_lib/
 	@#rsync -avpPxH $(NANOKVM_PRO_PACKAGE_DIR)/kvmapp/server/dl_lib/ /rootfs/kvmapp/server/dl_lib/
 	@rsync -avpPxH $(BUILDDIR)/bsp/axerabin/$(CHIP)/rootfs/opt/include/ /rootfs/opt/include/
+	@mkdir -p /rootfs/tmp/install/
 	@cp /output/$(CHIP_VENDOR)-middleware-$(BOARD)_*.deb /rootfs/tmp/install/
 	@chroot /rootfs bash -c 'dpkg -i /tmp/install/$(CHIP_VENDOR)-middleware-$(BOARD)_*.deb'
 	@[ "$(findstring libgpiod,$(IMAGE_ADDITIONS))" = "" ] || chroot /rootfs bash -c 'dpkg -i /tmp/install/libgpiod3_$(LIBGPIOD_VERSION)-$(LIBGPIOD_BUILD)_$(DEB_ARCH).deb'
