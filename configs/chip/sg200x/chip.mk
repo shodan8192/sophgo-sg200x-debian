eba950c5714628d173d24e6851f98dd98cf4c9a9
scpcom <scpcom@gmx.de>
2026-04-29 02:34:09 +0200

    middleware: update 6f5275b

--- configs/chip/sg200x/chip.mk
+++ configs/chip/sg200x/chip.mk
@@ -364,7 +364,7 @@ $(BUILDDIR)/middleware-prepare-clone-stamp:
 
 $(BUILDDIR)/middleware-prepare-checkout-root-stamp: $(BUILDDIR)/middleware-prepare-clone-stamp
 	@echo "$(COLOUR_GREEN)Checking out Middleware for $(BOARD)$(END_COLOUR)"
-	@cd $(BUILDDIR)/middleware && git checkout 6e8c433
+	@cd $(BUILDDIR)/middleware && git checkout 6f5275b
 	@cd $(BUILDDIR)/middleware && git submodule set-url 3rdparty/curl/curl $(GIT_USER_URL)/curl
 	@cd $(BUILDDIR)/middleware && git submodule set-url 3rdparty/ffmpeg/ffmpeg $(GIT_USER_URL)/FFmpeg
 	@cd $(BUILDDIR)/middleware && git submodule set-url 3rdparty/flatbuffers/flatbuffers $(GIT_USER_URL)/flatbuffers
